74b30566b59e77c5ca43821092b9b85508e546d0
Remy Goldschmidt <taktoa@gmail.com>
2015-06-03 22:35:35 -0500

    Initial commit.

---
+++ flycheck-pmd.el
@@ -0,0 +1,86 @@
+;;; flycheck-pmd.el --- Flycheck: PMD support    -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2015  Remy Goldschmidt <taktoa@gmail.com>
+
+;; Author: Remy Goldschmidt <taktoa@gmail.com>
+;; URL: https://github.com/taktoa/flycheck-pmd
+;; Keywords: convenience, tools, languages
+;; Version: 0.1-git
+;; Package-Requires: ((emacs "24.1") (flycheck "0.22") (let-alist "1.0.3"))
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; PMD support for Flycheck.
+
+;;; Code:
+
+(eval-when-compile
+  (require 'let-alist)
+  (require 'cl))
+
+(require 'flycheck)
+
+(defun pair-fold1 (proc seed ls)
+  "Fold the procedure (PROC) with a seed (SEED) over a list (LS)."
+  (loop
+     for e on ls by #'cdr
+     for acc = (funcall proc ls seed)
+     then (funcall proc e acc)
+     finally (return acc)))
+
+(defun single? (ls)
+  "Is the given list (LS) composed of a single element?"
+  (and (not (null ls))
+       (null (cdr ls))))
+
+(defun intersperse (item ls)
+  "Intersperse an element (ITEM) in a list (LS)."
+  (pair-fold1 #'(lambda (pr acc)
+                  (if (single? pr)
+                      (cons (car pr) acc)
+                      (cons item (cons (car pr) acc))))
+              '() (reverse ls)))
+
+(defconst
+  flycheck-pmd-rulesets
+  '("java-basic" "java-design")
+  "PMD rulesets to use.")
+
+(defconst
+  flycheck-pmd-args
+  (concat " -l java "
+          " -f emacs "
+          " -R " (apply 'concat (intersperse "," flycheck-pmd-rulesets)))
+  "Arguments for PMD error message.")
+
+(flycheck-define-checker java-pmd
+  "A syntax checker for Java using PMD."
+  :command ("pmd" "pmd" flycheck-pmd-args " -d " (file-name-directory source))
+  :error-patterns ((error line-start (file-name) ":" line ": " (message) line-end))
+  :modes '(java-mode))
+
+;;;###autoload
+(defun flycheck-pmd-setup ()
+  "Setup Flycheck PMD.
+Add `java-pmd' to `flycheck-checkers'."
+  (interactive)
+  (add-to-list 'flycheck-checkers 'java-pmd))
+
+(provide 'flycheck-pmd)
+
+;;; flycheck-pmd.el ends here
